a0a7bf235a3d853e27d8fb7e47ab7cb1f836f814
A2life <dutch@adachis.info>
2025-06-08 22:22:58 -0400

    beta2 package build

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -40,8 +40,8 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','Quip');
 define('PKG_NAME_LOWER','quip');
-define('PKG_VERSION','2.3.5a');
-define('PKG_RELEASE','pl');
+define('PKG_VERSION','2.3.5');
+define('PKG_RELEASE','modx3-beta2');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
